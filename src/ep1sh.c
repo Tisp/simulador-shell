85a53f41e2a66cd5a41f66e9a2a7adc4171deeec
Tisp <tisp.thiago@gmail.com>
2016-08-17 18:56:08 -0300

    adicionando mais alguns comentarios

--- src/ep1sh.c
+++ src/ep1sh.c
@@ -7,7 +7,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-
 #include "command.h"
 
 #define CWD_BUFFER 1024
@@ -18,10 +17,14 @@ int main(void) {
 	Command *command = NULL;
 	char *cwd;
 	char *rl;
-
+    
+	/* Pega o cwd do usuario */
 	cwd = getcwd(NULL, CWD_BUFFER);
+
+	/* Printa no terminar o cwd do usuario */
 	snprintf(prompt, CWD_BUFFER, "[%s] ", cwd);
 
+	/* Aguarda uma entrada do usuario */
 	while((rl = readline(prompt)) != NULL) {
 		
 		/* Encerra o shell */
@@ -39,14 +42,14 @@ int main(void) {
 			/* Verifica se foi passado 3 paramentros */
 			if(command->argc == 3) 
 		 		chmod(command->argv[2], atoi(command->argv[1]));
-			 else 
+			else 
 				printf("chmod uiltiliza dois parametros: chmod <modo numero> <arquivo> \n");
 		
-		/* Chamda de sistema para retornar o id do usuario */ 
+		/* Chamada de sistema para retornar o id do usuario */ 
 		} else if(strcmp(command->argv[0], "id") == 0 && strcmp(command->argv[1], "-u") == 0) {
 			printf("%d\n", getuid());
 		} else {
-			
+			/* Caso seja o filho executar o execve */
 			if(fork() == 0) {
 				
 				int r = execve(command->argv[0], command->argv, NULL); 
@@ -58,21 +61,31 @@ int main(void) {
 				}
 
 			} else {
+				/* Espero o filho retornar */
 				wait(NULL);
 			}
 		}
 
+		/* Adicionar o comando no historico do shell */
 		add_history(rl);
+
+		/* Libera memoria da string do comando */
 		free(rl);
 
+		/* Libera a memoria da string cwd */
 		free(cwd);
+	
+		/* Pega o cwd do usuario de novo e imprimi */
 		cwd = getcwd(NULL, CWD_BUFFER);
-		
 		snprintf(prompt, CWD_BUFFER, "(%s): ", cwd);
+		
+		/* Libera memoria da leitura do comando */
 		command_destroy(command);
 
 	}
-
+	
+	/* Libera memoria do readline */
 	free(rl);
+
 	return 0;
 }
